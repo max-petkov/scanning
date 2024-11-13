d73b7ad5243305e1edb16970902567e971e36a42
Maximilian Petkov <max@stan.vision>
2024-11-13 13:57:36 +0200

    working version

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ class Canvas {
     this.canvas = document.getElementById("hero-canvas");
     this.svg = document.getElementById("hero-svg");
     this.svgDots = this.svg.querySelectorAll("circle");
-    this._dots = [];
     this.dots = [];
     this.ctx = this.canvas.getContext("2d");
     this._size = {
@@ -18,14 +17,6 @@ class Canvas {
       y: null,
       r: (this._size.w / 80) * (this._size.h / 80),
     };
-  }
-
-  get dots() {
-    return this._dots;
-  }
-
-  set dots(val) {
-    return (this._dots = val);
   }
 
   get size() {
@@ -57,50 +48,166 @@ class Canvas {
     });
   }
 
-  draw() {
+  setDots() {
     this.svgDots.forEach((d, i) => {
       d.setAttribute("data-dot", i);
-      const x =
-        d.getBoundingClientRect().left +
-        (d.getBoundingClientRect().width / 2 +
-          (window.innerWidth - document.documentElement.clientWidth) / 1.5);
-      const y =
-        d.getBoundingClientRect().top + d.getBoundingClientRect().height / 2;
+      const rect = d.getBoundingClientRect();
+      const x = rect.left + (rect.width / 2 + (window.innerWidth - document.documentElement.clientWidth) / 1.5);
+      const y = rect.top + rect.height / 2;
       const r = Number(d.getAttribute("r"));
-      const id = i;
 
-      const dot = new Dot(id, y, r);
+      const dot = new Dot();
       dot.id = i;
       dot.x = x;
       dot.y = y;
       dot.r = r;
 
+      this.dots.push(dot);
 
-      gsap.to(dot, {
-        x: dot.x + Math.random() * 15 - 5, // Move slightly left/right
-        y: dot.y + Math.random() * 15 - 5, // Move slightly up/down
-        duration: 2 + Math.random() * 1, // Randomize duration
-        r: dot.r + Math.random() * 5 - 2.5,
-        repeat: -1, // Infinite loop
-        yoyo: true, // Return to original position
-        ease: "sine.inOut", // Smooth easing
-      });
-
-      this._dots.push(dot);
+      // Animation ⚡
+      // gsap.to(dot, {
+      //   x: dot.x + Math.random() * 15 - 5, // Move slightly left/right
+      //   y: dot.y + Math.random() * 15 - 5, // Move slightly up/down
+      //   duration: 2 + Math.random() * 1, // Randomize duration
+      //   r: dot.r + Math.random() * 5 - 2.5,
+      //   repeat: -1, // Infinite loop
+      //   yoyo: true, // Return to original position
+      //   ease: "sine.inOut", // Smooth easing
+      // });
     });
+
+    this.dots.forEach(d => d.dots = this.dots);
   }
 
   update() {
     gsap.ticker.add(() => {
       this.clear();
-      this._dots.forEach(d => d.update());
-      this.connect();
+      this.dots.forEach(d => d.update());
     });
   }
 
+  animate() {
+    this.dots.forEach(d => {
+      console.log(d);
+      
+    });
+  }
+
+
+  clear() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  init() {
+    this.setDots();
+    this.update();
+    this.animate();
+    this.resize();
+  }
+}
+
+class Dot extends Canvas {
+  constructor() {
+    super();
+    this._id = "";
+    this.id = "";
+
+    this._x = 0;
+    this.x = 0;
+
+    this._y = 0;
+    this.y = 0;
+
+    this._r = 0;
+    this.r = 0;
+
+    this._dots = [];
+
+    this._revealed = false;
+    this.revealed = false;
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  set x(val) {
+    return (this._x = val);
+  }
+
+  get y() {
+    return this._y;
+  }
+
+  set y(val) {
+    return (this._y = val);
+  }
+
+  get r() {
+    return this._r;
+  }
+
+  set r(val) {
+    return (this._r = val);
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  set id(val) {
+    return (this._id = val);
+  }
+
+  get dots() {
+    return this._dots;
+  }
+
+  set dots(val) {
+    return this._dots = val;
+  }
+
+  get revealed() {
+    return this._revealed;
+  }
+
+  set revealed(val) {
+    return this._revealed = val;
+  }
+
+  create() {
+    const gradient = this.ctx.createRadialGradient(
+      this._x,
+      this._y,
+      this._r * 0.1,
+      this._x,
+      this._y,
+      this._r
+    );
+
+    gradient.addColorStop(0, "rgba(255, 255, 255, 1)");
+    gradient.addColorStop(1, "rgba(255, 86, 0, 0)");
+
+    this.ctx.beginPath();
+    this.ctx.arc(this._x, this._y, this._r, 0, 2 * Math.PI);
+
+    if (
+      this._id === 16 ||
+      this._id === 15 ||
+      this._id === 14 ||
+      this._id === 13 ||
+      this._id === 18 ||
+      this._id === 8
+    )
+      this.ctx.fillStyle = "rgba(255, 255, 255, 0)";
+    else this.ctx.fillStyle = gradient;
+    this.ctx.fill();
+    this.ctx.closePath();
+  }
+
   connect() {
-    for (let i = 0; i < this._dots.length; i++) {
-      const dot = (i) => this._dots[i];
+    for (let i = 0; i < this.dots.length; i++) {
+      const dot = (i) => this.dots[i];
 
       this.line(dot(0), dot(1));
 
@@ -224,104 +331,11 @@ class Canvas {
     this.ctx.closePath();
   }
 
-  clear() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  }
-
-  init() {
-    this.draw();
-    this.resize();
-    this.update();
-  }
-}
-
-class Dot extends Canvas {
-  constructor() {
-    super();
-    this._id = "";
-    this.id = "";
-
-    this._x = 0;
-    this.x = 0;
-
-    this._y = 0;
-    this.y = 0;
-
-    this._r = 0;
-    this.r = 0;
-  }
-
-  get x() {
-    return this._x;
-  }
-
-  set x(val) {
-    return (this._x = val);
-  }
-
-  get y() {
-    return this._y;
-  }
-
-  set y(val) {
-    return (this._y = val);
-  }
-
-  get r() {
-    return this._r;
-  }
-
-  set r(val) {
-    return (this._r = val);
-  }
-
-  get id() {
-    return this._id;
-  }
-
-  set id(val) {
-    return (this._id = val);
-  }
-
-  create() {
-    const gradient = this.ctx.createRadialGradient(
-      this._x,
-      this._y,
-      this._r * 0.1,
-      this._x,
-      this._y,
-      this._r
-    );
-
-    gradient.addColorStop(0, "rgba(255, 255, 255, 1)");
-    gradient.addColorStop(1, "rgba(255, 86, 0, 0)");
-
-    this.ctx.beginPath();
-    this.ctx.arc(this._x, this._y, this._r, 0, 2 * Math.PI);
-
-    if (
-      this._id === 16 ||
-      this._id === 15 ||
-      this._id === 14 ||
-      this._id === 13 ||
-      this._id === 18 ||
-      this._id === 8
-    )
-      this.ctx.fillStyle = "rgba(255, 255, 255, 0)";
-    else this.ctx.fillStyle = gradient;
-    this.ctx.fill();
-    this.ctx.closePath();
-  }
-
   update() {
     this.create();
-  }
-
-  init() {
-    this.create();
+    this.connect();
   }
 }
-
 
 const canvas = new Canvas();
 canvas.init();
